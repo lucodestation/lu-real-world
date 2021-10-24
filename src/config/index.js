796b2de547f3d816394dca2aa63207df813fd4ca
卢忠伟 <lucodestation@163.com>
2021-10-24 13:33:06 +0800

    修复变量重复

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const requestBaseUrl = 'http://localhost:3030/api';
+// const requestBaseUrl = 'http://localhost:3030/api';
 const requestBaseUrl = 'https://lu-real-world-api-serverless.vercel.app/api';
 // const requestBaseUrl = 'https://conduit.productionready.io/api';
 // const requestBaseUrl = 'https://api.realworld.io/api';
