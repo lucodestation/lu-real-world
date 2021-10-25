05850fa138c1fa4437f2257aa3a3f91d5a7de44d
卢忠伟 <lucodestation@163.com>
2021-10-25 18:02:10 +0800

    改成线上接口

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-const requestBaseUrl = 'http://localhost:3030/api';
-// const requestBaseUrl = 'https://lu-real-world-api-serverless.vercel.app/api';
+// const requestBaseUrl = 'http://localhost:3030/api';
+const requestBaseUrl = 'https://lu-real-world-api-serverless.vercel.app/api';
 // const requestBaseUrl = 'https://conduit.productionready.io/api';
 // const requestBaseUrl = 'https://api.realworld.io/api';
 
