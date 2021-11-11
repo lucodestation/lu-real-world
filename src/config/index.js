8fd78d77ac2b6961a73c393119e02300f6f1c523
卢忠伟 <lucodestation@163.com>
2021-11-11 16:16:04 +0800

    测试成功

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 // const requestBaseUrl = 'http://localhost:3030/api';
 // const requestBaseUrl = 'https://lu-real-world-api-serverless.vercel.app/api';
-const requestBaseUrl = '/api';
+// const requestBaseUrl = '/api';
+const requestBaseUrl = 'https://lu-real-world.vercel.app/api';
 
 const tokenPrefix = 'Token ';
 
